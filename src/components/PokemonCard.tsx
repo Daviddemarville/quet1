d2ffb3bdedf4d91755d158c9fd3e4b062ce6695a
Daviddemarville <de.marville.david@gmail.com>
2025-03-31 22:39:19 +0200

    add components.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,10 +1,26 @@
-function PokemonCard() {
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  function PokemonCard() {
+    const pokemon = pokemonList[1]; // Récupère le premier Pokémon
+  
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption><h1>BulBasaur</h1></figcaption>
+          {pokemon.imgSrc ? (
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+            <p>???</p>
+          )}
         </figure>
-    );
-}
-
-export default PokemonCard;
+      );
+    }
+    
+    export default PokemonCard;
